96a76c9eed4ca14e81bc15c5970a6e75f813d2c9
willyvamp <williamrevenge1993@gmail.com>
2025-01-22 08:10:42 +0700

    add ship's movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,35 +11,52 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources"""
         pygame.init()
-        # defining the clock
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
-        # set the background color
-        self.bg_color = (230, 230, 230)
-        # set the ship
         self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-            # show the ship
-            self.ship.blitme()
-
-            # Make the recently drawn screen visible.
-            pygame.display.flip()
-            # make the loop run 60 times per second
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Response to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self.check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self.check_keyup_events(event)
+
+    def check_keydown_events(self, event):
+        """Respond to keypresses"""
+        if event.key == pygame.K_d:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_a:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_ESCAPE:
+            sys.exit()
+
+    def check_keyup_events(self, event):
+        """Respond to keyreleases"""
+        if event.key == pygame.K_d:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_a:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        """Update images on the screen and flip to the new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
--- ship.py
+++ ship.py
@@ -6,14 +6,28 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
-
         # load the ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-
         # start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+        # store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+        # movement flag; start not moving a ship
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update the ship's position based on movement flags"""
+        # Update the ship's x value; limiting the ship's range when it reaches the screen's edge
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+           self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+           self.x -= self.settings.ship_speed
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """draw the ship at its current location"""
